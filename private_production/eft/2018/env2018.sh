2f77164ce4008cf12d00190383bfedfd7a26f6ce
Flavia Cetorelli <f.cetorelli@campus.unimib.it>
2020-03-20 18:01:26 +0100

    Updated lines for WWOS production

--- private_production/eft/2018/env2018.sh
+++ private_production/eft/2018/env2018.sh
@@ -12,21 +12,21 @@ fi
 cd CMSSW_10_2_6/src
 eval `scram runtime -sh`
 
-curl -s --insecure https://cms-pdmv.cern.ch/mcm/public/restapi/requests/get_fragment/SMP-RunIIFall18wmLHEGS-00059 --retry 2 --create-dirs -o Configuration/GenProduction/python/SMP-RunIIFall18wmLHEGS-00059-fragment.py 
-[ -s Configuration/GenProduction/python/SMP-RunIIFall18wmLHEGS-00059-fragment.py ] || exit $?;
+curl -s --insecure https://cms-pdmv.cern.ch/mcm/public/restapi/requests/get_fragment/SMP-RunIIFall18wmLHEGS-00048 --retry 2 --create-dirs -o Configuration/GenProduction/python/SMP-RunIIFall18wmLHEGS-00048-fragment.py 
+[ -s Configuration/GenProduction/python/SMP-RunIIFall18wmLHEGS-00048-fragment.py ] || exit $?;
 
 scram b
 cd ../../
 seed=$(($(date +%s) % 100 + 1))
-cmsDriver.py Configuration/GenProduction/python/SMP-RunIIFall18wmLHEGS-00059-fragment.py --fileout file:SMP-RunIIFall18wmLHEGS-00059.root --mc --eventcontent RAWSIM,LHE --datatier GEN-SIM,LHE --conditions 102X_upgrade2018_realistic_v11 --beamspot Realistic25ns13TeVEarly2018Collision --step LHE,GEN,SIM --geometry DB:Extended --era Run2_2018 --python_filename SMP-RunIIFall18wmLHEGS-00059_1_cfg.py --no_exec --customise Configuration/DataProcessing/Utils.addMonitoring --customise_commands process.RandomNumberGeneratorService.externalLHEProducer.initialSeed="int(${seed})" -n 114 || exit $? ; 
+cmsDriver.py Configuration/GenProduction/python/SMP-RunIIFall18wmLHEGS-00048-fragment.py --fileout file:SMP-RunIIFall18wmLHEGS-00048.root --mc --eventcontent RAWSIM,LHE --datatier GEN-SIM,LHE --conditions 102X_upgrade2018_realistic_v11 --beamspot Realistic25ns13TeVEarly2018Collision --step LHE,GEN,SIM --geometry DB:Extended --era Run2_2018 --python_filename SMP-RunIIFall18wmLHEGS-00048_1_cfg.py --no_exec --customise Configuration/DataProcessing/Utils.addMonitoring --customise_commands process.RandomNumberGeneratorService.externalLHEProducer.initialSeed="int(${seed})" -n 114 || exit $? ; 
 
-cp SMP-RunIIFall18wmLHEGS-00059_1_cfg.py SMP-RunIIFall18wmLHEGS-00059_1_cfg_SM.py
-cp SMP-RunIIFall18wmLHEGS-00059_1_cfg.py SMP-RunIIFall18wmLHEGS-00059_1_cfg_INT.py
-cp SMP-RunIIFall18wmLHEGS-00059_1_cfg.py SMP-RunIIFall18wmLHEGS-00059_1_cfg_BSM.py
+cp SMP-RunIIFall18wmLHEGS-00048_1_cfg.py SMP-RunIIFall18wmLHEGS-00048_1_cfg_SM.py
+cp SMP-RunIIFall18wmLHEGS-00048_1_cfg.py SMP-RunIIFall18wmLHEGS-00048_1_cfg_INT.py
+cp SMP-RunIIFall18wmLHEGS-00048_1_cfg.py SMP-RunIIFall18wmLHEGS-00048_1_cfg_BSM.py
 
-sed "s/^.*args = cms.vstring(.*$/    args = cms.vstring('..\/SSWW_SMlimit_slc6_amd64_gcc630_CMSSW_9_3_16_tarball.tar.xz'),/" -i SMP-RunIIFall18wmLHEGS-00059_1_cfg_SM.py
-sed "s/^.*args = cms.vstring(.*$/    args = cms.vstring('..\/SSWW_RcW_int_slc6_amd64_gcc630_CMSSW_9_3_16_tarball.tar.xz'),/" -i SMP-RunIIFall18wmLHEGS-00059_1_cfg_INT.py
-sed "s/^.*args = cms.vstring(.*$/    args = cms.vstring('..\/SSWW_RcW_bsm_slc6_amd64_gcc630_CMSSW_9_3_16_tarball.tar.xz'),/" -i SMP-RunIIFall18wmLHEGS-00059_1_cfg_BSM.py
+sed "s/^.*args = cms.vstring(.*$/    args = cms.vstring('..\/OSWW_SMlimit_slc6_amd64_gcc630_CMSSW_9_3_16_tarball.tar.xz'),/" -i SMP-RunIIFall18wmLHEGS-00048_1_cfg_SM.py
+sed "s/^.*args = cms.vstring(.*$/    args = cms.vstring('..\/OSWW_RcW_int_slc6_amd64_gcc630_CMSSW_9_3_16_tarball.tar.xz'),/" -i SMP-RunIIFall18wmLHEGS-00048_1_cfg_INT.py
+sed "s/^.*args = cms.vstring(.*$/    args = cms.vstring('..\/OSWW_RcW_bsm_slc6_amd64_gcc630_CMSSW_9_3_16_tarball.tar.xz'),/" -i SMP-RunIIFall18wmLHEGS-00048_1_cfg_BSM.py
 
 echo '>>>>>>>>>>>>> set environment for Premix'
 export SCRAM_ARCH=slc6_amd64_gcc700
@@ -42,9 +42,9 @@ eval `scram runtime -sh`
 
 scram b
 cd ../../
-cmsDriver.py step1 --fileout file:SMP-RunIIAutumn18DRPremix-00048_step1.root  --pileup_input "dbs:/Neutrino_E-10_gun/RunIISummer17PrePremix-PUAutumn18_102X_upgrade2018_realistic_v15-v1/GEN-SIM-DIGI-RAW" --mc --eventcontent PREMIXRAW --datatier GEN-SIM-RAW --conditions 102X_upgrade2018_realistic_v15 --step DIGI,DATAMIX,L1,DIGI2RAW,HLT:@relval2018 --procModifiers premix_stage2 --nThreads 4 --geometry DB:Extended --datamix PreMix --era Run2_2018 --python_filename SMP-RunIIAutumn18DRPremix-00048_1_cfg.py --no_exec --customise Configuration/DataProcessing/Utils.addMonitoring -n 2626 || exit $? ; 
+cmsDriver.py step1 --fileout file:SMP-RunIIAutumn18DRPremix-00006_step1.root  --pileup_input "dbs:/Neutrino_E-10_gun/RunIISummer17PrePremix-PUAutumn18_102X_upgrade2018_realistic_v15-v1/GEN-SIM-DIGI-RAW" --mc --eventcontent PREMIXRAW --datatier GEN-SIM-RAW --conditions 102X_upgrade2018_realistic_v15 --step DIGI,DATAMIX,L1,DIGI2RAW,HLT:@relval2018 --procModifiers premix_stage2 --nThreads 4 --geometry DB:Extended --datamix PreMix --era Run2_2018 --python_filename SMP-RunIIAutumn18DRPremix-00006_1_cfg.py --no_exec --customise Configuration/DataProcessing/Utils.addMonitoring -n 2626 || exit $? ; 
 
-cmsDriver.py step2 --filein file:SMP-RunIIAutumn18DRPremix-00048_step1.root --fileout file:SMP-RunIIAutumn18DRPremix-00048.root --mc --eventcontent AODSIM --runUnscheduled --datatier AODSIM --conditions 102X_upgrade2018_realistic_v15 --step RAW2DIGI,L1Reco,RECO,RECOSIM,EI --procModifiers premix_stage2 --nThreads 4 --era Run2_2018 --python_filename SMP-RunIIAutumn18DRPremix-00048_2_cfg.py --no_exec --customise Configuration/DataProcessing/Utils.addMonitoring -n 2626 || exit $? ; 
+cmsDriver.py step2 --filein file:SMP-RunIIAutumn18DRPremix-00006_step1.root --fileout file:SMP-RunIIAutumn18DRPremix-00006.root --mc --eventcontent AODSIM --runUnscheduled --datatier AODSIM --conditions 102X_upgrade2018_realistic_v15 --step RAW2DIGI,L1Reco,RECO,RECOSIM,EI --procModifiers premix_stage2 --nThreads 4 --era Run2_2018 --python_filename SMP-RunIIAutumn18DRPremix-00006_2_cfg.py --no_exec --customise Configuration/DataProcessing/Utils.addMonitoring -n 2626 || exit $? ; 
 
 echo '>>>>>>>>>>>>> set environment for MINIAODSIM'
 export SCRAM_ARCH=slc6_amd64_gcc700
@@ -60,7 +60,7 @@ eval `scram runtime -sh`
 
 scram b
 cd ../../
-cmsDriver.py step1 --filein "dbs:/WpWpJJ_EWK_TuneCP5_13TeV-madgraph-pythia8/RunIIAutumn18DRPremix-102X_upgrade2018_realistic_v15-v1/AODSIM" --fileout file:SMP-RunIIAutumn18MiniAOD-00048.root --mc --eventcontent MINIAODSIM --runUnscheduled --datatier MINIAODSIM --conditions 102X_upgrade2018_realistic_v15 --step PAT --nThreads 4 --geometry DB:Extended --era Run2_2018 --python_filename SMP-RunIIAutumn18MiniAOD-00048_1_cfg.py --no_exec --customise Configuration/DataProcessing/Utils.addMonitoring -n 8597 || exit $? ; 
+cmsDriver.py step1 --filein "dbs:/WWJJToLNuLNu_EWK_noTop_13TeV-madgraph-pythia8/RunIIAutumn18MiniAOD-102X_upgrade2018_realistic_v15_ext1-v1/MINIAODSIM" --fileout file:SMP-RunIIAutumn18MiniAOD-00006.root --mc --eventcontent MINIAODSIM --runUnscheduled --datatier MINIAODSIM --conditions 102X_upgrade2018_realistic_v15 --step PAT --nThreads 4 --geometry DB:Extended --era Run2_2018 --python_filename SMP-RunIIAutumn18MiniAOD-00006_1_cfg.py --no_exec --customise Configuration/DataProcessing/Utils.addMonitoring -n 8597 || exit $? ; 
 
 echo '>>>>>>>>>>>>> set environment for NANOAODSIM'
 
@@ -77,5 +77,5 @@ eval `scram runtime -sh`
 
 scram b
 cd ../../
-cmsDriver.py step1 --filein "dbs:/WpWpJJ_EWK_TuneCP5_13TeV-madgraph-pythia8/RunIIAutumn18MiniAOD-102X_upgrade2018_realistic_v15-v1/MINIAODSIM" --fileout file:SMP-RunIIAutumn18NanoAODv5-00111.root --mc --eventcontent NANOAODSIM --datatier NANOAODSIM --conditions 102X_upgrade2018_realistic_v19 --step NANO --nThreads 2 --era Run2_2018,run2_nanoAOD_102Xv1 --python_filename SMP-RunIIAutumn18NanoAODv5-00111_1_cfg.py --no_exec --customise Configuration/DataProcessing/Utils.addMonitoring -n 10000 || exit $? ; 
-sed '/process.NANOAODSIMoutput = cms.OutputModule("NanoAODOutputModule",/a\    fakeNameForCrab =cms.untracked.bool(True),' -i SMP-RunIIAutumn18NanoAODv5-00111_1_cfg.py
+cmsDriver.py step1 --filein "dbs:/WWJJToLNuLNu_EWK_noTop_13TeV-madgraph-pythia8/RunIIAutumn18MiniAOD-102X_upgrade2018_realistic_v15_ext1-v1/MINIAODSIM" --fileout file:SMP-RunIIAutumn18NanoAODv5-00100.root --mc --eventcontent NANOAODSIM --datatier NANOAODSIM --conditions 102X_upgrade2018_realistic_v19 --step NANO --nThreads 2 --era Run2_2018,run2_nanoAOD_102Xv1 --python_filename SMP-RunIIAutumn18NanoAODv5-00100_1_cfg.py --no_exec --customise Configuration/DataProcessing/Utils.addMonitoring -n 10000 || exit $? ; 
+sed '/process.NANOAODSIMoutput = cms.OutputModule("NanoAODOutputModule",/a\    fakeNameForCrab =cms.untracked.bool(True),' -i SMP-RunIIAutumn18NanoAODv5-00100_1_cfg.py
